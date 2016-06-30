ed52dc82dae9a6a2e3b0d80e766d0b00b1481d36
Mohammd Sharifi <mmdsharifi@yahoo.com>
2016-06-30 12:08:24 +0430

    Update : Module Hello World!

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ createServer (req, res) : return an object that have listen method
 
 listen(PORT)
 */
+function start() {
+  function onRequest(request, response) {
+    console.log("Request received!");
+    response.writeHead( 200, {"Content-Type": "text/plain"} );
+    response.write("Hello World !");
+    response.end();
+  }
 
-function onRequest(request, response) {
-  console.log("Request received!");
-  response.writeHead( 200, {"Content-Type": "text/plain"} );
-  response.write("Hello World !");
-  response.end();
+  http.createServer( onRequest ).listen( 8888 );
+  console.log("Server has started !");
 }
 
-http.createServer( onRequest ).listen( 8888 );
-
-console.log(`Server Run on port 8888`);
+// export { start as fire };
+exports.fire = start;
